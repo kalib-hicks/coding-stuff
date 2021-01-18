19fd6711a51403805927d35ba2178f491757d031
Kalib <kalib.hicks@gmail.com>
2021-01-17 19:31:14 -0500

    inital commit

---
+++ prework/javascriptpractice/javascript.js
@@ -0,0 +1,47 @@
+    document.getElementById("button2").addEventListener("click", function(){
+
+        document.getElementById("box").style.backgroundColor = "blue";
+    
+    });
+
+    document.getElementById("button1").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "250px";
+
+    });
+
+    document.getElementById("button0").addEventListener("click", function(){
+
+        document.getElementById("box").style.border = "thick solid red";
+
+    });
+
+    document.getElementById("button3").addEventListener("click", function(){
+
+        document.getElementById("box").style.opacity = "0.5";
+
+    });
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+        document.getElementById("box").style.height = "150px";
+
+    });
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+        document.getElementById("box").style.opacity = "1.0";
+
+    });
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+        document.getElementById("box").style.backgroundColor = "orange";
+
+    });
+
+    document.getElementById("button4").addEventListener("click", function(){
+
+        document.getElementById("box").style.border = "orange";
+
+    });
